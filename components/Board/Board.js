942baa205c71c5e8d7e72cfbd31d3b914ffae672
LeoKnox <LeoKnox@users.noreply.github.com>
2025-04-14 15:04:18 -0700

    Update Board.js

--- components/Board/Board.js
+++ components/Board/Board.js
@@ -1,3 +1,13 @@
 export default Board = () => {
-  return "Board";
-};
+    const ranks = Array(8).fill().map((x,i) => 8-i)
+    const files = Array(8).fill().map((x,i) => i)
+  return (
+    <div>
+{ranks.map((rank,i) => 
+    files.map((file, j) =>
+        <div>{rank}{file}</div>
+    )
+)}
+        </div>
+  )
+}
