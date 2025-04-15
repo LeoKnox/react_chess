a2a94afd4006be1bf19fe6a6b88aa30b47b3a10b
LeoKnox <LeoKnox@users.noreply.github.com>
2025-04-15 15:51:29 -0700

    Update Board.js

--- components/Board/Board.js
+++ components/Board/Board.js
@@ -8,17 +8,22 @@ export default Board = () => {
   const files = Array(8)
     .fill()
     .map((x, i) => getCharacter(i));
+  const getClassName = (i, j) => {
+    console.log("get class name");
+    let c = "tile";
+    c += (i + j) % 2 === 0 ? " tile-brown" : " tile-beige";
+  };
   return (
     <div className="board">
-        <div className="tiles">
-      {ranks.map((rank, i) =>
-        files.map((file, j) => (
-          <div>
-            {rank}
-            {file}
-          </div>
-        ))
-      )}
+      <div className="tiles">
+        {ranks.map((rank, i) =>
+          files.map((file, j) => (
+            <div className={getClassName(i, j)}>
+              {rank}
+              {file}
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
