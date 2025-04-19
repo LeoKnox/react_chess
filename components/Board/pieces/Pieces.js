f7f0d3ecc5558aade90a920bd3b02dfd36a95984
LeoKnox <LeoKnox@users.noreply.github.com>
2025-04-18 17:43:40 -0700

    Update Pieces.js

--- components/Board/pieces/Pieces.js
+++ components/Board/pieces/Pieces.js
@@ -1,6 +1,6 @@
 import "./Pieces.css";
 
-expoet default const Pieces = () => {
+export const Pieces = () => {
     const position = new Array(8).fill('').map(Array(8).fill(''))
     console.log(position)
 }
