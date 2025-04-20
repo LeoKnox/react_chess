fab1deffff7be06e1084a11087cd0b35d176a8b1
LeoKnox <LeoKnox@users.noreply.github.com>
2025-04-19 19:21:55 -0700

    Update Pieces.js

--- components/Board/pieces/Pieces.js
+++ components/Board/pieces/Pieces.js
@@ -6,6 +6,16 @@ const Pieces = () => {
     .map(() => Array(8).fill(""));
   position[0][0] = "wr";
   console.log(position);
+  return (
+    <div>
+        {position.map((r,rank) =>
+            r.map((f,file) =>
+                position[rank][file]
+                ? position[rank][file]
+                : null
+            ))}
+        </div>
+  )
 };
 
 export default Pieces;
