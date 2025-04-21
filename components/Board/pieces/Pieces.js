a32c1cd8ef74254de14bca30da8ee9e909d78f2f
LeoKnox <LeoKnox@users.noreply.github.com>
2025-04-20 19:14:21 -0700

    Update Pieces.js

--- components/Board/pieces/Pieces.js
+++ components/Board/pieces/Pieces.js
@@ -1,6 +1,7 @@
 import "./Pieces.css";
+import { Piece } from "./Piece.js";
 
-export default Pieces = () => {
+const Pieces = () => {
   const position = Array(8)
     .fill(null)
     .map(() => Array(8).fill(""));
@@ -9,8 +10,19 @@ export default Pieces = () => {
   return (
     <div className="pieces">
       {position.map((r, rank) =>
-        r.map((f, file) => (position[rank][file] ? <Piece key={rank+":"+file} rank={rank} file={file} piece={position[rank][file]} /> : null))
+        r.map((f, file) =>
+          position[rank][file] ? (
+            <Piece
+              key={rank + ":" + file}
+              rank={rank}
+              file={file}
+              piece={position[rank][file]}
+            />
+          ) : null
+        )
       )}
     </div>
   );
 };
+
+export default Pieces;
